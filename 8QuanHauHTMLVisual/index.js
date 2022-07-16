b8c722a331fb8e672a91509393ae7330198795b3
lamminhthien <lamminhthien02012000@gmail.com>
2022-07-16 21:23:47 +0700

    add simulator graphics in console first

--- 8QuanHauHTMLVisual/index.js
+++ 8QuanHauHTMLVisual/index.js
@@ -1,11 +1,32 @@
 var currentPath = window.location.pathname;
 currentPath = currentPath.replace("index.html","result.txt")
 console.log(currentPath);
-rawData = ""
+
 async function fetchText() {
   let response = await fetch(currentPath);
-  let data = await response.text();
-  console.log(data.split("\n"));
+  let resultArray = await (await response.text()).split("\n");
+  resultArray.forEach(item => {
+    var listPositionForQueen = item.split('')
+    listPositionForQueen.pop()
+    listPositionForQueen = listPositionForQueen.map(elem => parseInt(elem,10))
+    console.log('begin of this method');
+    drawChessBoardAndQueen(listPositionForQueen)
+    console.log('end of this method');
+  })
 }
 
+function drawChessBoardAndQueen(data) {
+  var element = document.querySelector('.chess-board');
+  for (let i = 0; i <8; i++) {
+    var rowGraphicsSimulator =''
+    for (let j=1; j<=8;j++) {
+      if (data[i]==j) rowGraphicsSimulator = rowGraphicsSimulator + "X"
+      else rowGraphicsSimulator = rowGraphicsSimulator + "-"
+    }
+    console.log(rowGraphicsSimulator);
+  }
+}
+
+// Run graphics simulate
 fetchText()
+

638d69145f9f060bfb8c6cff7523b26d9d586814
lamminhthien <lamminhthien02012000@gmail.com>
2022-07-16 09:33:14 +0000

    rename variable trò chơi bốc sỏi

--- TroChoiBocSoi_TimKetQua/Program.cs
+++ TroChoiBocSoi_TimKetQua/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 // n là số lượng viên sỏi
-// m là số lượng viên sỏi tối đa có thể bốc được trong 1 lượt
-static int Ket(int n, int m) {
+// k là số lượng viên sỏi tối đa có thể bốc được trong 1 lượt
+static int Ket(int n, int k) {
   // 0 là thắng, 1 là thua
-  return (n % (m+1) == 1 ? 0 : 1);
+  return (n % (k+1) == 1 ? 0 : 1);
 }
 
-static int CachDi(int n,int m) {
-  int r = n % (m+1);
+static int CachDi(int n,int k) {
+  // Đoạn dư r = n mod (k+1)
+  int r = n % (k+1);
   if (r == 1) return 1;
-  else return (r==0 ? m : r-1);
+  else return (r==0 ? k : r-1);
 }
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine(CachDi(13,3));
+Console.WriteLine(CachDi(13,2));

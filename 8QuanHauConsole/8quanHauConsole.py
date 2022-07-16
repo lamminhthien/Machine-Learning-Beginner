d96543c049ae7c4b07fd818bf02fea51b7d92ab3
lamminhthien <lamminhthien02012000@gmail.com>
2022-07-16 04:40:39 +0000

    init thuật toán 8 quân hậu đơn luồng

---
+++ 8QuanHauConsole/8quanHauConsole.py
@@ -0,0 +1,28 @@
+a = [None]*20
+def Ok(x2,y2):
+  # kiểm tra cách đặt có thỏa mãn không
+  for i in range (1,x2):
+    if (a[i] == y2 or abs(i-x2) == abs(a[i]-y2)):
+      return bool(0)
+  # Nếu kiểm tra hết các trường hợp vẫn không sai thì trả về true
+  return bool(1)
+
+def Xuat(n):
+  # In ra một kết quả
+  for i in range(1,n):
+    print(f'{a[i]}')
+  print('\n')
+
+def Try(i,n):
+  for j in range(1,n+1):
+    # Thử đặt quân hậu vào các cột từ 1 đến n
+    if(Ok(i,j)):
+      # Nếu cách đặt này thỏa mãn thì lưu lại vị trí
+      a[i] = j
+      # Nếu đặt xong quân hậu thứ n thì xuất ra một kết quả
+      if (i==n):
+        Xuat(n)
+      Try(i+1,n)
+
+n = 8 # Ở đây mình cho bài toán là 8 quân hậu trên bàn 8*8
+Try(1,n)

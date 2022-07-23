92af08ac364d85d4b43853dfba84994c0828388d
lamminhthien <lamminhthien02012000@gmail.com>
2022-07-23 07:46:05 +0700

    .

--- NvidiaCUDA/vector_add.c
+++ NvidiaCUDA/vector_add.c
@@ -2,21 +2,22 @@
 #include <math.h>
 
 // function to add the elements of two arrays
- void add(int n, float *x, float *y)
+void add(int n, float *x, float *y)
 {
   for (int i = 0; i < n; i++)
-      y[i] = x[i] + y[i];
+    y[i] = x[i] + y[i];
 }
 
 int main(void)
 {
-  int N = 1<<20; // 1M elements
+  int N = 1 << 20; // 1M elements
 
   float *x = new float[N];
   float *y = new float[N];
 
   // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++)
+  {
     x[i] = 1.0f;
     y[i] = 2.0f;
   }
@@ -27,12 +28,12 @@ int main(void)
   // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
   for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
+    maxError = fmax(maxError, fabs(y[i] - 3.0f));
   std::cout << "Max error: " << maxError << std::endl;
 
   // Free memory
-  delete [] x;
-  delete [] y;
+  delete[]x;
+  delete[]y;
 
   return 0;
 }
